29d1143bdd63ea64841325572b4291a2df888f7b
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2022-11-13 11:34:26 +0200

    test1

--- Lessons-13-05.11/lesson/app.js
+++ Lessons-13-05.11/lesson/app.js
@@ -1,4 +1,29 @@
-alert("jjjjj");
-alert()
+// let enterYourMonth = prompt("Month");
 
+// if (
+//   (enterYourMonth == "декабрь") |
+//   (enterYourMonth == "январь") |
+//   (enterYourMonth == "феврать")
+// ) {
+//   console.log("Is winter");
+// } else if (
+//   (enterYourMonth == "март") |
+//   (enterYourMonth == "апрель") |
+//   (enterYourMonth == "май")
+// ) {
+//   console.log("Is spring");
+// } else if (
+//   (enterYourMonth == "июнь") |
+//   (enterYourMonth == "июль") |
+//   (enterYourMonth == "август")
+// ) {
+//   console.log("Is summer");
+// } else if (
+//   (enterYourMonth == "сентябрь") |
+//   (enterYourMonth == "октябрь") |
+//   (enterYourMonth == "ноябрь")
+// ) {
+//   console.log("Is autumn");
+// }
 
+console.log("10" && true && 0 && null);
