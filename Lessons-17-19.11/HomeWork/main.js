a2d6a4b9a7fb57352fad3d5070c2e5af3f17ae4e
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2022-12-03 00:04:28 +0200

    hw 17

---
+++ Lessons-17-19.11/HomeWork/main.js
@@ -0,0 +1,12 @@
+// let arr = prompt("Enter many numbers");
+let arr = "1,4,2,5,8,6,9,7,3";
+let arr2 = new Array;
+for (let i = 0; i < arr.length; i++) {
+  arr2=arr[i].split();
+}
+
+// arr2.sort(function (b, a) {
+//   return b - a;
+// });
+console.log(arr2);
+// alert(`${arr2}`);
