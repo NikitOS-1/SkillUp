900767eefc727c8395e5d217fa0ff6eddb2c21b9
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2022-12-03 00:25:57 +0200

    finish hw17 js

--- Lessons-17-19.11/HomeWork/main.js
+++ Lessons-17-19.11/HomeWork/main.js
@@ -1,12 +1,26 @@
-// let arr = prompt("Enter many numbers");
-let arr = "1,4,2,5,8,6,9,7,3";
-let arr2 = new Array;
-for (let i = 0; i < arr.length; i++) {
-  arr2=arr[i].split();
-}
+// - Завдання 2. Отримати від користувача набір чисел через кому,
+// і відсортувати їх від найбільшого до найменшого
 
-// arr2.sort(function (b, a) {
+// let arr = prompt("Enter many numbers");
+// let arr2 = arr.split(",");
+
+// arr2.sort(function (a, b) {
 //   return b - a;
 // });
-console.log(arr2);
+
 // alert(`${arr2}`);
+
+// - Завдання 3. Визначити найбільше/найменше число в довільному числовому масиві
+
+// let numArray = [1, 2, 54, 2, 32, 64556, 342, 1, 32, 354];
+// console.log(Math.max.apply(null, numArray));
+// console.log(Math.min.apply(null, numArray));
+
+// Завдання 4. Порахувати суму елементів в довільному числовому масиві
+
+// let numArray = [1, 2, 3];
+// let sumArray = 0;
+// for (i = 0; i < numArray.length; i++) {
+//   sumArray += numArray[i];
+// }
+// console.log(sumArray);
