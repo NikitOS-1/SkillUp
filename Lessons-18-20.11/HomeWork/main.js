ff46c466cfecb2709ace03048a218206a007991f
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2022-12-03 13:17:23 +0200

    hw 18 start

--- Lessons-18-20.11/HomeWork/main.js
+++ Lessons-18-20.11/HomeWork/main.js
@@ -1,26 +1,10 @@
-// - Завдання 2. Отримати від користувача набір чисел через кому,
-// і відсортувати їх від найбільшого до найменшого
+// - Завдання 1. Написати функцію, яка приймає два параметри trimString(str, maxlength). Функція перевіряє довжину
+// рядка str, і якщо вона перевершує maxlength - замінює кінець str на "..."
 
-// let arr = prompt("Enter many numbers");
-// let arr2 = arr.split(",");
+let str = "wejkkdssdlk";
 
-// arr2.sort(function (a, b) {
-//   return b - a;
-// });
+function trimString(str, maxlength) {
+    console.log(str.length())
+}
 
-// alert(`${arr2}`);
-
-// - Завдання 3. Визначити найбільше/найменше число в довільному числовому масиві
-
-// let numArray = [1, 2, 54, 2, 32, 64556, 342, 1, 32, 354];
-// console.log(Math.max.apply(null, numArray));
-// console.log(Math.min.apply(null, numArray));
-
-// Завдання 4. Порахувати суму елементів в довільному числовому масиві
-
-// let numArray = [1, 2, 3];
-// let sumArray = 0;
-// for (i = 0; i < numArray.length; i++) {
-//   sumArray += numArray[i];
-// }
-// console.log(sumArray);
+trimString(str, 2);
