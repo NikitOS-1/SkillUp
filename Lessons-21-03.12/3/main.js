e77b90741e5e733f4fab34988c88415e2035e231
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2022-12-16 19:01:47 +0200

    test

--- Lessons-21-03.12/3/main.js
+++ Lessons-21-03.12/3/main.js
@@ -1,10 +1,17 @@
 // ----------------fetch-rest-api
 
-fetch("https://jsonplaceholder.typicode.com/posts")
+// fetch("https://jsonplaceholder.typicode.com/posts")
+//   .then((response) => response.json())
+//   .then((json) =>
+//     json.map((item) => {
+//       let title = item.title;
+//       console.log(title);
+//     })
+//   );
+// http://api.openweathermap.org/data/2.5/weather?q=KYIV&units=metric&APPID=5d066958a60d315387d9492393935c19
+
+fetch(
+  "http://api.openweathermap.org/data/2.5/weather?q=KYIV&units=metric&APPID=5d066958a60d315387d9492393935c19"
+)
   .then((response) => response.json())
-  .then((json) =>
-    json.map((item) => {
-      let title = item.title;
-      console.log(title);
-    })
-  );
+  .then((json) => console.log(json));
